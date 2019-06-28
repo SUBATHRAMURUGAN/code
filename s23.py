b6eb6140588d01f9412fbedaed3dfa6cce1a94af
SUBATHRAMURUGAN <51949909+SUBATHRAMURUGAN@users.noreply.github.com>
2019-06-28 15:36:37 +0530

    Update s23.py

--- s23.py
+++ s23.py
@@ -1,7 +1,7 @@
-number = int(input())
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
+getnum = int(input())
+if getnum > 1:
+    for s in range(2, getnum):
+        if (getnum % s) == 0:
             print("no")
             break
     else:
