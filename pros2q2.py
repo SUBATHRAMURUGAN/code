19bdd4897d99637b5345fc0b3394768f9be59bf0
SUBATHRAMURUGAN <51949909+SUBATHRAMURUGAN@users.noreply.github.com>
2019-07-20 14:12:22 +0530

    Update pros2q2.py

--- pros2q2.py
+++ pros2q2.py
@@ -1,8 +1,8 @@
 inp,opt=map(int,input().split())
-li=list(map(int,input().split()[:inp]))
+l=list(map(int,input().split()[:inp]))
 aah=[]
 for ii in range(opt):
 	you,we=list(map(int,input().split()))
-    aah.append(sum(li[you-1:we]))
+	aah.append(sum(l[you-1:we]))
 for jj in aah:
 	print(jj)
