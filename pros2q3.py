1eb1ef5961faba71cc8d5b0ecf7d4e5e1db3b287
SUBATHRAMURUGAN <51949909+SUBATHRAMURUGAN@users.noreply.github.com>
2019-07-20 14:36:40 +0530

    Create pros2q3.py

---
+++ pros2q3.py
@@ -0,0 +1,8 @@
+inp,opt=map(int,input().split())
+l=list(map(int,input().split()[:inp]))
+aah=[]
+for ii in range(opt):
+	you,we=list(map(int,input().split()))
+	aah.append(min(l[you-1:we]))
+for jj in aah:
+	print(jj)
